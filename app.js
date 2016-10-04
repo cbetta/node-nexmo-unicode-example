d3b0061a94d1b173a12805590e9cf3fb92deda1a
Cristiano Betta <cbetta@gmail.com>
2016-10-04 19:22:24 +0100

    Add Nexmo init

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var nexmo = require('nexmo');
+var Nexmo = require('nexmo');
+
+nexmo = new Nexmo({
+  apiKey: "yyy",
+  apiSecret: "xxx"
+}, {
+  debug: true
+});
 
 var regex = /â|ê|î|ô|û|ë|ï|ç|ù/g;
 
